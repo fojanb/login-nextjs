f4e25612a5a32b0caf713cf5572fe2fb0394ac2e
Fojan Babaali <fozhanmail@gmail.com>
2021-06-03 23:38:46 -0700

    Update auth.js

--- login-nextjs/lib/auth.js
+++ login-nextjs/lib/auth.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+// Or: const axios = require("axios")
+// Axios ---> promise based HTTP request
 // Login the User by its username and password which returns the Response as a Promise
 export const loginUser = async (username, password) => {
 	const { data } = await axios.post("/api/login", { username, password });
@@ -8,5 +9,5 @@ export const loginUser = async (username, password) => {
 };
 
 
-// Axios for HTTP request
-// ----
+
+
