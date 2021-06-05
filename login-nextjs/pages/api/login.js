48c474f2aadbd9778ef2ed51ba663664e8ea683c
Fojan Babaali <fozhanmail@gmail.com>
2021-06-05 09:59:22 -0700

    chore: minor improvements

--- login-nextjs/pages/api/login.js
+++ login-nextjs/pages/api/login.js
@@ -1,15 +1,15 @@
 import { login } from "../../lib/users";
 
 export default (req, res) => {
-	if (req.method !== "POST") {
-		res.status(404).json({
-			error: "METHOD_NOT_ALLOWED",
-			message: `${req.method} is not allowed, please use the POST http method.`,
-		});
-		return;
-	}
+  if (req.method !== "POST") {
+    res.status(405).json({
+      error: "METHOD_NOT_ALLOWED",
+      message: `${req.method} is not allowed, please use the POST http method.`,
+    });
+    return;
+  }
 
-	const { username, password } = JSON.parse(req.body);
+  const { username, password } = JSON.parse(req.body);
 
-	res.status(200).json(login(username, password));
+  res.status(200).json(login(username, password));
 };
--- login-nextjs/pages/api/profile.js
+++ login-nextjs/pages/api/profile.js
@@ -1,30 +1,31 @@
 import { findUser, login, register, verifyToken } from "../../lib/users";
 
 export default (req, res) => {
-	if (req.method !== "GET") {
-		res.status(404).json({
-			error: "METHOD_NOT_ALLOWED",
-			message: `${req.method} is not allowed, please use the GET http method.`,
-		});
-		return;
-	}
-	const authorizationToken = req.headers.authorization;
-	const isVerified = verifyToken(authorizationToken);
+  if (req.method !== "GET") {
+    res.status(404).json({
+      error: "METHOD_NOT_ALLOWED",
+      message: `${req.method} is not allowed, please use the GET http method.`,
+    });
+    return;
+  }
+  const authorizationToken = req.headers.authorization;
+  const verifiedUser = verifyToken(authorizationToken);
 
-	if (isVerified && isVerified.has) {
-		console.log(isVerified.username);
-		const user = findUser(isVerified.username);
-		res.status(200).json({
-			payload: {
-				username: user.username,
-				email: user.email,
-				id: user.id,
-			},
-		});
-	}
+  console.log("username", verifiedUser.username);
+  if (trueTypeOf(verifiedUser) === "object" && verifiedUser.hasOwnProperty("username")) {
+    const user = findUser(verifiedUser.username);
+    res.status(200).json({
+      payload: {
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      },
+    });
+    return;
+  }
 
-	res.status(404).json({
-		error: "WRONG_CREDENTIAL",
-		message: "Your are not allowed to see this API.",
-	});
+  res.status(401).json({
+    error: "UNAUTHORIZED",
+    message: "Not allowed.",
+  });
 };
