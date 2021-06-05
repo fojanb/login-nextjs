763ac7d8a999892deb26a4c1903651791451dc20
Fojan Babaali <fozhanmail@gmail.com>
2021-06-04 22:29:48 -0700

    Update auth.js

--- login-nextjs/lib/auth.js
+++ login-nextjs/lib/auth.js
@@ -4,26 +4,25 @@ export const loginUser = async (username, password) => {
 		method: "POST",
 	});
 	const data = await res.json();
-
 	return data;
 };
+// ------------------------------------------------------------*
 export const registerUser = async (payload) => {
 	const res = await fetch("/api/register", {
 		body: JSON.stringify(payload),
 		method: "POST",
 	});
 	const data = await res.json();
-
 	return data;
 };
+// ------------------------------------------------------------*
 export const whoAmI = async () => {
 	const res = await fetch("/api/profile", {
 		headers: {
-			authorization: window.localStorage.getItem("token"),
+		authorization: window.localStorage.getItem("token"),
 		},
 		method: "GET",
 	});
 	const data = await res.json();
-
 	return data;
 };
