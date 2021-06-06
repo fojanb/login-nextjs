04b336f719244424f2fe3ad527b636f0f570114b
Fojan Babaali <fozhanmail@gmail.com>
2021-06-05 21:37:31 -0700

    Update index.js

--- login-nextjs/components/Auth/Login/index.js
+++ login-nextjs/components/Auth/Login/index.js
@@ -1,72 +1,90 @@
 import React from "react";
+import Router from "next/router";
 import { loginUser } from "../../../lib/auth";
 
 export function LoginForm() {
-	const [username, setUsername] = React.useState("");
-	const [password, setPassword] = React.useState("");
-	const [rememberMe, setRememberMe] = React.useState(false);
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [rememberMe, setRememberMe] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
 
-	async function handleSubmit(e) {
-		e.preventDefault();
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-		console.log(username, password, rememberMe);
-		try {
-			// API call:
-			const data = await loginUser(username, password);
+    console.log(username, password, rememberMe);
+    try {
+      setIsLoading(true);
+      // API call:
+      const data = await loginUser(username, password);
 
-			if (data.payload && data.payload.token) {
-				window.localStorage.setItem("token", data.payload.token);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
+      if (data.payload && data.payload.token) {
+        if (rememberMe) {
+          window.localStorage.setItem("token", data.payload.token);
+        } else {
+          window.sessionStorage.setItem("token", data.payload.token);
+        }
+        Router.push("/dashboard");
+      } else {
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<fieldset>
-				<legend className="h1">Login</legend>
-				<div className="mb-3">
-					<label htmlFor="usernameInput" className="form-label">
-						Username
-					</label>
-					<input
-						type="text"
-						id="usernameInput"
-						className="form-control"
-						placeholder="Username"
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-				</div>
-				<div className="mb-3">
-					<label htmlFor="passwordInput" className="form-label">
-						Password
-					</label>
-					<input
-						type="password"
-						id="passwordInput"
-						className="form-control"
-						placeholder="Password"
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</div>
-				<div className="mb-3">
-					<div className="form-check">
-						<input
-							className="form-check-input"
-							type="checkbox"
-							id="RememberMeInput"
-							onChange={(e) => setRememberMe(e.target.checked)}
-						/>
-						<label className="form-check-label" htmlFor="RememberMeInput">
-							Remember Me
-						</label>
-					</div>
-				</div>
-				<button type="submit" className="btn btn-primary">
-					Submit
-				</button>
-			</fieldset>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend className="h1">Login</legend>
+        <div className="mb-3">
+          <label htmlFor="usernameInput" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            id="usernameInput"
+            className="form-control"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="passwordInput" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="passwordInput"
+            className="form-control"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="RememberMeInput"
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <label className="form-check-label" htmlFor="RememberMeInput">
+              Remember Me
+            </label>
+          </div>
+        </div>
+        {errorMessage && (
+          <div className="alert alert-danger" role="alert">
+            {errorMessage}
+          </div>
+        )}
+        <button type="submit" className="btn btn-primary" disabled={isLoading}>
+          Submit
+        </button>
+      </fieldset>
+    </form>
+  );
 }
