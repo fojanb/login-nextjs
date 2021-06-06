24dff3cafeace10494e0ae767b4622b44db9f14d
Fojan Babaali <fozhanmail@gmail.com>
2021-06-05 21:37:39 -0700

    Update auth.js

--- login-nextjs/lib/auth.js
+++ login-nextjs/lib/auth.js
@@ -1,28 +1,28 @@
 export const loginUser = async (username, password) => {
-	const res = await fetch("/api/login", {
-		body: JSON.stringify({ username, password }),
-		method: "POST",
-	});
-	const data = await res.json();
-	return data;
+  const res = await fetch("/api/login", {
+    body: JSON.stringify({ username, password }),
+    method: "POST",
+  });
+  const data = await res.json();
+  return data;
 };
 // ------------------------------------------------------------*
 export const registerUser = async (payload) => {
-	const res = await fetch("/api/register", {
-		body: JSON.stringify(payload),
-		method: "POST",
-	});
-	const data = await res.json();
-	return data;
+  const res = await fetch("/api/register", {
+    body: JSON.stringify(payload),
+    method: "POST",
+  });
+  const data = await res.json();
+  return data;
 };
 // ------------------------------------------------------------*
 export const whoAmI = async () => {
-	const res = await fetch("/api/profile", {
-		headers: {
-		authorization: window.localStorage.getItem("token"),
-		},
-		method: "GET",
-	});
-	const data = await res.json();
-	return data;
+  const res = await fetch("/api/profile", {
+    headers: {
+      authorization: window.localStorage.getItem("token"),
+    },
+    method: "GET",
+  });
+  const data = await res.json();
+  return data;
 };
