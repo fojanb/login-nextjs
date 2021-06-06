03aed263486a1058c2cfc04125915ec3b3c8a798
Fojan Babaali <fozhanmail@gmail.com>
2021-06-05 21:37:46 -0700

    Update profile.js

--- login-nextjs/pages/api/profile.js
+++ login-nextjs/pages/api/profile.js
@@ -1,31 +1,43 @@
+import { trueTypeOf } from "../../helpers";
 import { findUser, login, register, verifyToken } from "../../lib/users";
 
 export default (req, res) => {
   if (req.method !== "GET") {
-    res.status(404).json({
+    res.status(403).json({
       error: "METHOD_NOT_ALLOWED",
       message: `${req.method} is not allowed, please use the GET http method.`,
     });
     return;
   }
   const authorizationToken = req.headers.authorization;
-  const verifiedUser = verifyToken(authorizationToken);
+  if (authorizationToken) {
+    try {
+      const verifiedUser = verifyToken(authorizationToken);
 
-  console.log("username", verifiedUser.username);
-  if (trueTypeOf(verifiedUser) === "object" && verifiedUser.hasOwnProperty("username")) {
-    const user = findUser(verifiedUser.username);
-    res.status(200).json({
-      payload: {
-        username: user.username,
-        email: user.email,
-        id: user.id,
-      },
-    });
-    return;
+      if (trueTypeOf(verifiedUser) === "object" && verifiedUser.hasOwnProperty("username")) {
+        const user = findUser(verifiedUser.username);
+        res.status(200).json({
+          payload: {
+            username: user.username,
+            email: user.email,
+            id: user.id,
+          },
+        });
+        return;
+      }
+    } catch (error) {
+      // Token has been expired or we detected a fruad attack
+      res.status(401).json({
+        error: "Unauthorized",
+        message: "Not allowed.",
+      });
+      return;
+    }
   }
 
+  // Token has been expired or we detected a fruad attack
   res.status(401).json({
-    error: "UNAUTHORIZED",
+    error: "Unauthorized",
     message: "Not allowed.",
   });
 };
