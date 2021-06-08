466c56afb1d855be04924939f790bf3afb077f3b
Fojan Babaali <fozhanmail@gmail.com>
2021-06-08 14:02:29 -0700

    Update login.js

--- login-nextjs/pages/auth/login.js
+++ login-nextjs/pages/auth/login.js
@@ -1,10 +1,19 @@
 import { BaseAuthLayout } from "../../components/Auth/Base";
 import { LoginForm } from "../../components/Auth/Login";
+import Link from "next/link";
 
+const styles = {
+  marginTop: 30,
+  textAlign: "center",
+};
 export default function Login() {
   return (
     <BaseAuthLayout>
       <LoginForm />
+
+      <div style={styles}>
+        <Link href="/auth/register">Signup now!</Link>
+      </div>
     </BaseAuthLayout>
   );
 }
