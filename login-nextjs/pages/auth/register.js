59f9b943ae47a53015e39db9211e52b31a48db87
Fojan Babaali <fozhanmail@gmail.com>
2021-06-08 14:02:31 -0700

    Update register.js

--- login-nextjs/pages/auth/register.js
+++ login-nextjs/pages/auth/register.js
@@ -1,10 +1,19 @@
 import { BaseAuthLayout } from "../../components/Auth/Base";
 import { RegisterForm } from "../../components/Auth/Register";
+import Link from "next/link";
 
+const styles = {
+  marginTop: 30,
+  textAlign: "center",
+};
 export default function Register() {
   return (
     <BaseAuthLayout>
       <RegisterForm />
+
+      <div style={styles}>
+        <Link href="/auth/login">Registered Before? Signin Now!</Link>
+      </div>
     </BaseAuthLayout>
   );
 }
