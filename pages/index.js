66159c59c58dc81432ac704d82b307e1c0753be0
Fojan Babaali <fozhanmail@gmail.com>
2021-06-08 23:26:57 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Link from "next/link";
 const Homepage = () => {
+  const styles = {
+    width: "40%",
+    justifyContent: "center",
+    margin: "100px auto",
+    padding: "40px",
+    backgroundColor :""
+  };
   return (
-    <div>
-      <h3>Welcome Soldier</h3>
-      <Link href="auth/login">Login</Link>
-      <Link href="auth/register">Register</Link>
+    <div className="card" style={styles}>
+      {/* <img src="..." className="card-img-top" alt="..." /> */}
+      <div className="card-body">
+        <h5 className="card-title">Welcome Soldier!</h5>
+        <p className="card-text">
+          If you've already registered then please log in, else please register.
+        </p>
+        <Link href="auth/login">
+          <a className="btn btn-primary" style={{marginRight:"30px"}}>Login</a>
+        </Link>
+        <Link href="auth/register">
+          <a className="btn btn-primary">Register</a>
+        </Link>
+      </div>
     </div>
   );
 };
